a21bfbf8bb690edb50a62bb3ebccbfd5e395d8ff
clauder69 <57950770+clauder69@users.noreply.github.com>
2024-12-31 10:26:38 -0300

    Create OledExpressions.h

---
+++ OledExpressions.h
@@ -0,0 +1,16 @@
+#ifndef OLEDEXPRESSIONS_H
+#define OLEDEXPRESSIONS_H
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+extern Adafruit_SSD1306 display;
+
+void desenharOlhosTriste();
+void desenharOlhosAlegre();
+void desenharOlhosBravo();
+void desenharOlhosRosnar();
+void desenharOlhosUivar();
+void desenharOlhosChoro();
+
+#endif
